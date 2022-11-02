01d63eba1c048d6923c5e73a2dd877911d50c7b4
Doomwatcher2004 <70971867+Doomwatcher2004@users.noreply.github.com>
2022-11-02 12:21:27 +0800

    Update GulimallProductApplicationTests.java

--- gulimall-product/src/test/java/com/hyc/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/hyc/gulimall/product/GulimallProductApplicationTests.java
@@ -57,14 +57,14 @@ public class GulimallProductApplicationTests {
     @Test
     public void testUpload() {
         //// Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
-        //String endpoint = "oss-cn-guangzhou.aliyuncs.com";
+        //String endpoint = "xxxxx";
         //// 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        //String accessKeyId = "LTAI5tHP7fX71ZpFiyx4bw69";
-        //String accessKeySecret = "4N0qRDrFLdDbvYWOPaI4Soc6CDTOKG";
+        //String accessKeyId = "xxxxx";
+        //String accessKeySecret = "xxxxx";
         // 填写Bucket名称，例如examplebucket。
-        String bucketName = "gulimall-doomwatcher";
+        String bucketName = "xxxxx";
         // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
-        String objectName = "gulimall/jiale1.png";
+        String objectName = "xxxxx";
         // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
         // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
         String filePath = "C:\\Users\\doomwstcher.000\\Pictures\\QQ截图20220513000450.png";
